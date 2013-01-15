d40494e409b04739c84ab571f2de0da7475c79a9
Ray Viljoen <ray@fubra.com>
2013-01-15 10:18:51 +0000

    Changes version number to be configured in single location

--- osx_php_update.sh
+++ osx_php_update.sh
@@ -14,13 +14,21 @@ read ans
 sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
 ln -s /opt/X11 /usr/X11
 
+# =================================================
+# Update download version here
+# Should simply increment on mirror site
+PHP_VERS="5.4.10"
+# =================================================
+
 # Src target
 PHP_SRC_DEST="${HOME}/Downloads/"
 
-PHP_SRC_DIR="${PHP_SRC_DEST}"php-5.4.6
+PHP_SRC_DIR="${PHP_SRC_DEST}php-${PHP_VERS}"
 
-# Download link. Update if needed. Currently at 5.4.6 (And obviously stick with a .tar.gz)
-PHP_SRC_LINK="http://uk3.php.net/get/php-5.4.10.tar.gz/from/this/mirror"
+echo $PHP_SRC_DIR
+
+# Download link. Update if needed. (stick with a .tar.gz)
+PHP_SRC_LINK="http://uk3.php.net/get/php-${PHP_VERS}.tar.gz/from/this/mirror"
 
 # Check for homebrew
 command -v brew >/dev/null 2>&1 || { echo >&2 "
